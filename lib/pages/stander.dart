89a6c2dd5b0bdb87af0c4458af910bd56c5a8cb3
U2yyy <a1535887812@gmail.com>
2025-06-30 14:45:47 +0800

    利用虚拟无限索引解决轮播图最后一张图重回第一张图的抽搐问题

--- lib/pages/stander.dart
+++ lib/pages/stander.dart
@@ -12,6 +12,7 @@ class Stander extends StatefulWidget {
 class _StanderState extends State<Stander> {
   Timer? _timer;
   int _currentPage = 0;
+  List<Widget> children = [];
   final PageController _pageController = PageController();
   final List<String> imagePaths = [
     'assets/images/jojo1.jpg',
@@ -26,15 +27,11 @@ class _StanderState extends State<Stander> {
   void initState() {
     super.initState();
     _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
-      if (_currentPage < imagePaths.length - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
+      _currentPage++;
       _pageController.animateToPage(
         _currentPage,
         duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
+        curve: Curves.easeInOut,
       );
     });
   }
@@ -48,24 +45,30 @@ class _StanderState extends State<Stander> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children = [];
-    for (var path in imagePaths) {
-      children.add(
-        Align(
-          alignment: Alignment.topCenter,
-          child: ClipOval(
-            child: SizedBox(
-              width: 200,
-              height: 200,
-              child: Image.asset(path, fit: BoxFit.cover),
-            ),
-          ),
-        ),
-      );
-    }
     return Scaffold(
-      appBar: AppBar(title: Text('轮播图demo')),
-      body: PageView(controller: _pageController, children: children),
+      appBar: AppBar(title: Text('无限轮播图demo')),
+      body: ConstrainedBox(
+        constraints: BoxConstraints(minHeight: 300),
+        child: PageView.builder(
+          controller: _pageController,
+          onPageChanged: (index) {
+            _currentPage = index;
+          },
+          itemBuilder: (context, index) {
+            String path = imagePaths[index % imagePaths.length];
+            return Align(
+              alignment: Alignment.topCenter,
+              child: ClipOval(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(path, fit: BoxFit.cover),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
